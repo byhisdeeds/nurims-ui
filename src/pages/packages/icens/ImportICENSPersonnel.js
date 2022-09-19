03fd543e0eb4b634605e1410d681d8dc0274375d
John Preston <byhisdeeds@gmail.com>
2022-09-19 18:12:26 -0500

    Added code to import personnel from a json file.

--- src/pages/packages/icens/ImportICENSPersonnel.js
+++ src/pages/packages/icens/ImportICENSPersonnel.js
@@ -133,22 +133,7 @@ class ImportICENSPersonnel extends Component {
     if (message.hasOwnProperty("response")) {
       const response = message.response;
       if (response.hasOwnProperty("status") && response.status === 0) {
-        if (message.hasOwnProperty("cmd") && message.cmd ===CMD_GET_PERSONNEL_RECORDS) {
-          if (message.hasOwnProperty(INCLUDE_METADATA)) {
-            const selection = this.state.selection;
-            const personnel = getMatchingResponseObject(message, "response.personnel", "item_id", selection["item_id"]);
-            selection[NURIMS_TITLE] = personnel[NURIMS_TITLE];
-            selection[NURIMS_WITHDRAWN] = personnel[NURIMS_WITHDRAWN];
-            selection[METADATA] = [...personnel[METADATA]]
-            if (this.pmref.current) {
-              this.pmref.current.set_person_object(selection);
-            }
-          } else {
-            if (this.plref.current) {
-              this.plref.current.add(response.personnel, true);
-            }
-          }
-        } else if (message.hasOwnProperty("cmd") && message.cmd === CMD_UPDATE_PERSONNEL_RECORD) {
+        if (message.hasOwnProperty("cmd") && message.cmd === CMD_UPDATE_PERSONNEL_RECORD) {
           toast.success(`Personnel record for ${response.personnel[NURIMS_TITLE]} updated successfully`)
           if (this.plref.current) {
             this.plref.current.update(response.personnel);
@@ -156,17 +141,6 @@ class ImportICENSPersonnel extends Component {
           // if (this.pmref.current) {
           //   this.pmref.current.set_metadata(response.personnel);
           // }
-        } else if (message.hasOwnProperty("cmd") && message.cmd === "permanently_delete_person") {
-          toast.success("Personnel record deleted successfully")
-          if (this.plref.current) {
-            this.plref.current.removePerson(this.state.selection)
-          }
-          // if (this.plref.current) {
-          //   this.plref.current.update_selected_person(response.personnel)
-          // }
-          if (this.pmref.current) {
-            this.pmref.current.update_personnel_details({})
-          }
         }
       } else {
         toast.error(response.message);
@@ -174,26 +148,26 @@ class ImportICENSPersonnel extends Component {
     }
   }
 
-  onPersonSelected = (selection) => {
-    // console.log("-- onPersonSelected (previous selection) --", previous_selection)
-    console.log("-- onPersonSelected (selection) --", selection)
-    if (selection.hasOwnProperty("item_id") && selection.item_id === -1) {
-      if (this.pmref.current) {
-        this.pmref.current.set_person_object(selection)
-      }
-    } else {
-      this.setState(pstate => {
-        return { selection: selection }
-      });
-      this.props.send({
-        cmd: CMD_GET_PERSONNEL_RECORDS,
-        item_id: selection.item_id,
-        "include.metadata": "true",
-        module: MODULE,
-      });
-    }
-    this.setState({ selection: selection })
-  }
+  // onPersonSelected = (selection) => {
+  //   // console.log("-- onPersonSelected (previous selection) --", previous_selection)
+  //   console.log("-- onPersonSelected (selection) --", selection)
+  //   if (selection.hasOwnProperty("item_id") && selection.item_id === -1) {
+  //     if (this.pmref.current) {
+  //       this.pmref.current.set_person_object(selection)
+  //     }
+  //   } else {
+  //     this.setState(pstate => {
+  //       return { selection: selection }
+  //     });
+  //     this.props.send({
+  //       cmd: CMD_GET_PERSONNEL_RECORDS,
+  //       item_id: selection.item_id,
+  //       "include.metadata": "true",
+  //       module: MODULE,
+  //     });
+  //   }
+  //   this.setState({ selection: selection })
+  // }
 
   saveChanges = () => {
     if (this.plref.current) {
@@ -222,75 +196,75 @@ class ImportICENSPersonnel extends Component {
     this.setState({metadata_changed: false})
   }
 
-  addPerson = () => {
-    if (this.plref.current) {
-      this.plref.current.add([{
-        "changed": true,
-        "item_id": -1,
-        "nurims.title": "New Person",
-        "nurims.withdrawn": 0,
-        "metadata": []
-      }], false);
-      this.setState({ changed: true });
-    }
-  }
+  // addPerson = () => {
+  //   if (this.plref.current) {
+  //     this.plref.current.add([{
+  //       "changed": true,
+  //       "item_id": -1,
+  //       "nurims.title": "New Person",
+  //       "nurims.withdrawn": 0,
+  //       "metadata": []
+  //     }], false);
+  //     this.setState({ changed: true });
+  //   }
+  // }
 
-  onMetadataChanged = (state) => {
-    this.setState({metadata_changed: state});
-  }
+  // onMetadataChanged = (state) => {
+  //   this.setState({metadata_changed: state});
+  // }
 
-  proceed_with_selection_change = () => {
-    // set new selection and load details
-    // console.log("#### saving personnel details ###", this.state.previous_selection)
-    this.setState({alert: false, metadata_changed: false});
-    if (this.plref.current) {
-      this.plref.current.setSelection(this.state.selection)
-    }
-    if (this.state.selection.hasOwnProperty("item_id") && this.state.selection.item_id === -1) {
-      if (this.pmref.current) {
-        this.pmref.current.set_metadata(this.state.selection)
-      }
-    } else {
-      this.props.send({
-        cmd: 'get_personnel_metadata',
-        item_id: this.state.selection.item_id,
-        module: MODULE,
-      });
-    }
-  }
+  // proceed_with_selection_change = () => {
+  //   // set new selection and load details
+  //   // console.log("#### saving personnel details ###", this.state.previous_selection)
+  //   this.setState({alert: false, metadata_changed: false});
+  //   if (this.plref.current) {
+  //     this.plref.current.setSelection(this.state.selection)
+  //   }
+  //   if (this.state.selection.hasOwnProperty("item_id") && this.state.selection.item_id === -1) {
+  //     if (this.pmref.current) {
+  //       this.pmref.current.set_metadata(this.state.selection)
+  //     }
+  //   } else {
+  //     this.props.send({
+  //       cmd: 'get_personnel_metadata',
+  //       item_id: this.state.selection.item_id,
+  //       module: MODULE,
+  //     });
+  //   }
+  // }
 
-  cancel_selection_change = () => {
-    this.setState({alert: false,});
-    if (this.plref.current) {
-      this.plref.current.setSelection(this.state.previous_selection)
-    }
-  }
+  // cancel_selection_change = () => {
+  //   this.setState({alert: false,});
+  //   if (this.plref.current) {
+  //     this.plref.current.setSelection(this.state.previous_selection)
+  //   }
+  // }
 
-  removePerson = () => {
-    this.setState({confirm_remove: true,});
-  }
+  // removePerson = () => {
+  //   this.setState({confirm_remove: true,});
+  // }
 
-  cancel_remove = () => {
-    this.setState({confirm_remove: false,});
-  }
+  // cancel_remove = () => {
+  //   this.setState({confirm_remove: false,});
+  // }
 
-  proceed_with_remove = () => {
-    this.setState({confirm_remove: false,});
-    console.log("REMOVE PERSON", this.state.selection);
-    if (this.state.selection.item_id === -1) {
-      if (this.plref.current) {
-        this.plref.current.removePerson(this.state.selection)
-      }
-    } else {
-      this.props.send({
-        cmd: 'permanently_delete_person',
-        item_id: this.state.selection.item_id,
-        "nurims.title": this.state.selection["nurims.title"],
-        "nurims.withdrawn": this.state.selection["nurims.withdrawn"],
-        module: MODULE,
-      });
-    }
-  }
+  // proceed_with_remove = () => {
+  //   this.setState({confirm_remove: false,});
+  //   console.log("REMOVE PERSON", this.state.selection);
+  //   if (this.state.selection.item_id === -1) {
+  //     if (this.plref.current) {
+  //       this.plref.current.removePerson(this.state.selection)
+  //     }
+  //   } else {
+  //     this.props.send({
+  //       cmd: 'permanently_delete_person',
+  //       item_id: this.state.selection.item_id,
+  //       "nurims.title": this.state.selection["nurims.title"],
+  //       "nurims.withdrawn": this.state.selection["nurims.withdrawn"],
+  //       module: MODULE,
+  //     });
+  //   }
+  // }
 
   handleFileUpload = (e) => {
     const selectedFile = e.target.files[0];
