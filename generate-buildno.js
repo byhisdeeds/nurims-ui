5a3309433c409285ea33699cb491f2127170ec28
John Preston <byhisdeeds@gmail.com>
2023-02-24 14:19:09 -0500

    Fixed up authorize reactor sample irradiations code

--- generate-buildno.js
+++ generate-buildno.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
-
+const METADATA_JSON = "src/metadata.json";
+let metadata = {"buildMajor":1,"buildMinor":0,"buildRevision":"20230201.2216","buildTag":"BETA"}
 console.log('Incrementing build number...');
-fs.readFile('src/metadata.json', function (err, content) {
+if (fs.existsSync(METADATA_JSON)) {
+  metadata = JSON.parse(fs.readFileSync('src/metadata.json'));
+}
+// metadata.buildRevision = metadata.buildRevision + 1;
+metadata.buildRevision = new Date().toISOString()
+  .replace(/-/ig, '')
+  .replace(/T/, '.')
+  .replace(/:/g, '')
+  .substring(0, 13);
+fs.writeFile(METADATA_JSON, JSON.stringify(metadata), function (err) {
   if (err) throw err;
-  const metadata = JSON.parse(content);
-  // metadata.buildRevision = metadata.buildRevision + 1;
-  metadata.buildRevision = new Date().toISOString()
-    .replace(/-/ig, '')
-    .replace(/T/, '-')
-    .replace(/:/g, '')
-    .substring(0, 13);
-  fs.writeFile('src/metadata.json', JSON.stringify(metadata), function (err) {
-    if (err) throw err;
-    console.log(`Current build number: ${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildRevision} ${metadata.buildTag}`);
-  })
-});
+  console.log(`Current build number: ${metadata.buildRevision} ${metadata.buildTag}`);
+})
