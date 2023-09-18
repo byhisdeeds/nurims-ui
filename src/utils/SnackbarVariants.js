61bf42ad8465f084eb590f1243def2eb0b722861
John Preston <byhisdeeds@gmail.com>
2023-09-18 17:21:53 -0500

    Fixed up enqueSnackbar code to add auto_close parameter

--- src/utils/SnackbarVariants.js
+++ src/utils/SnackbarVariants.js
@@ -1,7 +1,7 @@
 import {enqueueSnackbar} from "notistack";
 
 export function enqueueWarningSnackbar(message, duration, auto_close) {
-  const hideAfter = (duration || 2000) * (auto_close) ? 5 : 1
+  const hideAfter = (duration ? duration : 3500) * (auto_close ? 5 : 1)
   enqueueSnackbar(message,  {
     variant: 'warning',
     autoHideDuration: hideAfter,
@@ -9,7 +9,7 @@ export function enqueueWarningSnackbar(message, duration, auto_close) {
 }
 
 export function enqueueInfoSnackbar(message, duration, auto_close) {
-  const hideAfter = (duration || 2000) * (auto_close) ? 5 : 1
+  const hideAfter = (duration ? duration : 2000) * (auto_close ? 5 : 1)
   enqueueSnackbar(message,  {
     variant: 'info',
     autoHideDuration: hideAfter,
@@ -17,7 +17,7 @@ export function enqueueInfoSnackbar(message, duration, auto_close) {
 }
 
 export function enqueueErrorSnackbar(message, duration, auto_close) {
-  const hideAfter = (duration || 2000) * (auto_close) ? 5 : 1
+  const hideAfter = (duration ? duration : 5000) * (auto_close ? 5 : 1)
   enqueueSnackbar(message,  {
     variant: 'error',
     autoHideDuration: hideAfter,
@@ -25,7 +25,7 @@ export function enqueueErrorSnackbar(message, duration, auto_close) {
 }
 
 export function enqueueSuccessSnackbar(message, duration, auto_close) {
-  const hideAfter = (duration || 2000) * (auto_close) ? 5 : 1
+  const hideAfter = (duration ? duration : 2000) * (auto_close ? 5 : 1)
   enqueueSnackbar(message, {
       variant: 'success',
       autoHideDuration: hideAfter,
