60b19f26341ef89d1647020b5a7bf5c04aabd5d2
John Preston <byhisdeeds@gmail.com>
2023-09-18 16:27:29 -0500

    Updated enqueSnackbar code to add auto hide duration parameter

--- src/utils/SnackbarVariants.js
+++ src/utils/SnackbarVariants.js
@@ -1,17 +1,17 @@
 import {enqueueSnackbar} from "notistack";
 
-export function enqueueWarningSnackbar(message) {
-  enqueueSnackbar(message, { variant: 'warning' });
+export function enqueueWarningSnackbar(message, duration) {
+  enqueueSnackbar(message, { variant: 'warning', autoHideDuration: duration|| 2000 });
 }
 
-export function enqueueInfoSnackbar(message) {
-  enqueueSnackbar(message, { variant: 'info' });
+export function enqueueInfoSnackbar(message, duration) {
+  enqueueSnackbar(message, { variant: 'info', autoHideDuration: duration || 1000 });
 }
 
-export function enqueueErrorSnackbar(message) {
-  enqueueSnackbar(message, { variant: 'error' });
+export function enqueueErrorSnackbar(message, duration) {
+  enqueueSnackbar(message, { variant: 'error', autoHideDuration: duration|| 2000 });
 }
 
-export function enqueueSuccessSnackbar(message) {
-  enqueueSnackbar(message, { variant: 'success' });
+export function enqueueSuccessSnackbar(message, duration) {
+  enqueueSnackbar(message, { variant: 'success', autoHideDuration: duration|| 1000 });
 }
