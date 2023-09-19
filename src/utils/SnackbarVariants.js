f976a22ea20214bbe5ded8d1f98efe151c6f916e
jp <john.preston@uwimona.edu.jm>
2023-09-19 10:16:10 -0500

    Updated background messages duration before hiding

--- src/utils/SnackbarVariants.js
+++ src/utils/SnackbarVariants.js
@@ -1,33 +1,40 @@
 import {enqueueSnackbar} from "notistack";
 
 export function enqueueWarningSnackbar(message, duration, auto_close) {
-  const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
-  enqueueSnackbar(message,  {
+  // const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
+  const hideAfter = (auto_close || false) ? (duration || 2000) : 10000
+  console.log("hide warning", hideAfter)
+  enqueueSnackbar(message, {
     variant: 'warning',
     autoHideDuration: hideAfter,
   });
 }
 
 export function enqueueInfoSnackbar(message, duration, auto_close) {
-  const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
-  enqueueSnackbar(message,  {
+  // const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
+  const hideAfter = (auto_close || false) ? (duration || 2000) : 10000
+  console.log("hide info", hideAfter)
+  enqueueSnackbar(message, {
     variant: 'info',
     autoHideDuration: hideAfter,
   });
 }
 
 export function enqueueErrorSnackbar(message, duration, auto_close) {
-  const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
-  enqueueSnackbar(message,  {
+  // const hideAfter =  (duration || 2000) * ((auto_close || false) ? 5 : 1)
+  const hideAfter = (auto_close || false) ? (duration || 2000) : 10000
+  enqueueSnackbar(message, {
     variant: 'error',
     autoHideDuration: hideAfter,
   });
 }
 
 export function enqueueSuccessSnackbar(message, duration, auto_close) {
-  const hideAfter = (duration || 2000) * ((auto_close || false) ? 5 : 1)
+  // const hideAfter = (duration || 2000) * ((auto_close || false) ? 5 : 1)
+  const hideAfter = (auto_close || false) ? (duration || 2000) : 10000
+  console.log("hide success", hideAfter)
   enqueueSnackbar(message, {
-      variant: 'success',
-      autoHideDuration: hideAfter,
-    });
+    variant: 'success',
+    autoHideDuration: hideAfter,
+  });
 }
