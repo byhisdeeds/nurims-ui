1386d55a9a598590da290b23a1c9128931fbde96
jp <john.preston@uwimona.edu.jm>
2023-11-03 14:55:10 -0500

    Fixed up code

--- src/pages/maintenance/SSCMetadata.js
+++ src/pages/maintenance/SSCMetadata.js
@@ -294,7 +294,7 @@ class SSCMetadata extends Component {
         autoComplete="off"
       >
         <Card variant={"outlined"} style={{marginBottom: 8}} sx={{m: 0, pl: 0, pb: 0, width: '100%'}}>
-          <CardHeader title={"General Details"} titleTypographyProps={{fontSize: 20}}/>
+          <CardHeader title={"General Details"} titleTypographyProps={{fontSize: c}}/>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
@@ -382,7 +382,7 @@ class SSCMetadata extends Component {
           </CardContent>
         </Card>
         <Card variant="outlined" style={{marginBottom: 8}} sx={{m: 0, pl: 0, pb: 0, width: '100%'}}>
-          <CardHeader title={"Reactor Safety"} titleTypographyProps={{fontSize: 20}}/>
+          <CardHeader title={"Reactor Safety"} titleTypographyProps={{fontSize: "1.5em"}}/>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
@@ -436,7 +436,7 @@ class SSCMetadata extends Component {
           </CardContent>
         </Card>
         <Card variant="outlined" style={{marginBottom: 8}} sx={{m: 0, pl: 0, pb: 0, width: '100%'}}>
-          <CardHeader title={"Operations"} titleTypographyProps={{fontSize: 18}}/>
+          <CardHeader title={"Operations"} titleTypographyProps={{fontSize: "1.5em"}}/>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
@@ -526,7 +526,7 @@ class SSCMetadata extends Component {
           </CardContent>
         </Card>
         <Card variant="outlined" style={{marginBottom: 8}} sx={{m: 0, pl: 0, pb: 0, width: '100%'}}>
-          <CardHeader title={"Surveillance"} titleTypographyProps={{fontSize: 18}}/>
+          <CardHeader title={"Surveillance"} titleTypographyProps={{fontSize: "1.5em"}}/>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12}>
