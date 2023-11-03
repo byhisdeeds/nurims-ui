89c5266ba63604dd7134aca6c435cf7641a04e0e
John Preston <byhisdeeds@gmail.com>
2023-11-03 17:32:44 -0500

    Fixed code

--- src/pages/maintenance/SSCMetadata.js
+++ src/pages/maintenance/SSCMetadata.js
@@ -294,7 +294,7 @@ class SSCMetadata extends Component {
         autoComplete="off"
       >
         <Card variant={"outlined"} style={{marginBottom: 8}} sx={{m: 0, pl: 0, pb: 0, width: '100%'}}>
-          <CardHeader title={"General Details"} titleTypographyProps={{fontSize: "1.5em"}} sx={{pt: 1, pl: 3, pb: 0}}/>
+          <CardHeader title={"Basic Details"} titleTypographyProps={{fontSize: "1.5em"}} sx={{pt: 1, pl: 3, pb: 0}}/>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
