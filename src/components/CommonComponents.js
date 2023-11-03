47fe30271c2490ab10c26071fe1d5a86c9a7fbd5
jp <john.preston@uwimona.edu.jm>
2023-11-03 15:55:55 -0500

    Fixed up code

--- src/components/CommonComponents.js
+++ src/components/CommonComponents.js
@@ -342,6 +342,7 @@ export function SelectFormControlWithTooltip({
       </Floater>
       <Select
         disabled={disabled}
+        sx={{'& .MuiTextField-root': {margin: 0}}}
         required
         fullWidth
         multiple={multiple}
@@ -402,6 +403,7 @@ export function TextFieldWithTooltip({
   return (
     <Box style={{paddingRight: padding, marginTop: padding, width: '100%'}}>
       <TextField
+        sx={{'& .MuiTextField-root': {margin: 0}}}
         defaultValue={" "}
         disabled={disabled}
         required={required}
@@ -501,9 +503,6 @@ export function DatePickerWithTooltip({
             disabled={disabled}
             label={
               <Floater
-                // callback={cb}
-                // component={TooltipContent}
-                // target={target}
                 content={tooltip}
                 showCloseButton={true}
                 hideArrow
