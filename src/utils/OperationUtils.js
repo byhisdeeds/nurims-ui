26529f9eab26b67bfeab778d0034a2e0e1cce843
John Preston <byhisdeeds@gmail.com>
2023-11-29 18:38:44 -0500

    Fixed code

--- src/utils/OperationUtils.js
+++ src/utils/OperationUtils.js
@@ -31,20 +31,185 @@ export function prepareExportData(message) {
     blobData: "",
   }
   const data = message.response.operation;
+  const datasetFormat = message.hasOwnProperty("datasetFormat") ? message.datasetFormat : "json"
   const startDate = message.hasOwnProperty("startDate") ? message.startDate : "0000-00"
   const endDate = message.hasOwnProperty("endDate") ? message.endDate.substring(5,7) : "00"
   const dataset = message.hasOwnProperty("dataset") ? message.dataset : ""
   if (dataset === "stats") {
-    f.fileName = `operating-run-${dataset}-${startDate}-${endDate}.json`;
-    f.fileType = "application/json";
-    const records = [];
-    for (const record of data) {
-      const _d = getRecordMetadataValue(record, NURIMS_OPERATION_DATA_STATS, "");
-      records.push(_d)
+    if (datasetFormat === "json") {
+      f.fileName = `operating-run-${dataset}-${startDate}-${endDate}.json`;
+      f.fileType = "application/json";
+      const records = [];
+      for (const record of data) {
+        const _d = getRecordMetadataValue(record, NURIMS_OPERATION_DATA_STATS, "");
+        records.push(_d)
+      }
+      f.blobData = JSON.stringify(records,null, 2);
+    } else if (datasetFormat === "csv") {
+      f.fileName = `operating-run-${dataset}-${startDate}-${endDate}.csv`;
+      f.fileType = "text/csv";
+      const data_array = [];
+      for (const record of data) {
+        data_array.push(getRecordMetadataValue(record, NURIMS_OPERATION_DATA_STATS, ""));
+      }
+      const header = Object.keys(data_array[0]).join(",");
+      // Setup values from object values
+      // const values = data_array.map(item => Object.values(Array.isArray(item)? {}:item).join(","));
+      // const values = data_array.reduce( (acc, val) =>
+      //   acc.concat( Object.values(Array.isArray(val) ? [] : val).join(`;`) ), [] );
+      const values = data_array.map(row => {
+        if (row.hasOwnProperty("rod_events")) {
+          console.log("ROD-EVENTS: ", row)
+          return {}
+        }if (row.hasOwnProperty("flux")) {
+          console.log("FLUX: ", row)
+          return {}
+        } else {
+          console.log(">>>>", row)
+          return {...row}
+        }
+      })
+
+      // Concat header and values with a linebreak
+      f.blobData = [header, ...values].join("\n");
     }
-    f.blobData = JSON.stringify(records,null, 2);
   } else if (dataset === "flux") {
 
   }
   return f;
+}
+
+/**
+ * Converts an array of objects into a CSV file.
+ */
+class ObjectsToCsv {
+  /**
+   * Creates a new instance of the object array to csv converter.
+   * @param {object[]} objectArray
+   */
+  constructor(objectArray) {
+    if (!Array.isArray(objectArray)) {
+      throw new Error('The input to objects-to-csv must be an array of objects.');
+    }
+
+    if (objectArray.length > 0) {
+      if (objectArray.some(row => typeof row !== 'object')) {
+        throw new Error('The array must contain objects, not other data types.');
+      }
+    }
+
+    this.data = objectArray;
+  }
+
+  /**
+   * Saves the CSV file to the specified file.
+   * @param {string} filename - The path and filename of the new CSV file.
+   * @param {object} options - The options for writing to disk.
+   * @param {boolean} [options.append] - Whether to append to file. Default is overwrite (false).
+   * @param {boolean} [options.bom] - Append the BOM mark so that Excel shows
+   * @param {boolean} [options.allColumns] - Whether to check all items for column names or only the first.  Default is the first.
+   * Unicode correctly.
+   */
+  async toDisk(filename, options) {
+    if (!filename) {
+      throw new Error('Empty filename when trying to write to disk.');
+    }
+
+    let addHeader = false;
+
+    // If the file didn't exist yet or is empty, add the column headers
+    // as the first line of the file. Do not add it when we are appending
+    // to an existing file.
+    const fileNotExists = !fs.existsSync(filename) || fs.statSync(filename).size === 0;
+    if (fileNotExists || !options || !options.append) {
+      addHeader = true;
+    }
+
+    const allColumns = options && options.allColumns
+      ? options.allColumns
+      : false;
+
+    let data = await this.toString(addHeader, allColumns);
+    // Append the BOM mark if requested at the beginning of the file, otherwise
+    // Excel won't show Unicode correctly. The actual BOM mark will be EF BB BF,
+    // see https://stackoverflow.com/a/27975629/6269864 for details.
+    if (options && options.bom && fileNotExists) {
+      data = '\ufeff' + data;
+    }
+
+    if (options && options.append) {
+      return new Promise((resolve, reject) => {
+        fs.appendFile(filename, data, 'utf8', (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, 'utf8', (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    }
+  }
+
+  /**
+   * Returns the CSV file as string.
+   * @param {boolean} header - If false, omit the first row containing the
+   * column names.
+   * @param {boolean} allColumns - Whether to check all items for column names.
+   *   Uses only the first item if false.
+   * @returns {Promise<string>}
+   */
+  async toString(header = true, allColumns = false) {
+    return await convert(this.data, header, allColumns);
+  }
+}
+
+/**
+ * Private method to run the actual conversion of array of objects to CSV data.
+ * @param {object[]} data
+ * @param {boolean} header - Whether the first line should contain column headers.
+ * @param {boolean} allColumns - Whether to check all items for column names.
+ *   Uses only the first item if false.
+ * @returns {string}
+ */
+async function convert(data, header = true, allColumns = false) {
+  if (data.length === 0) {
+    return '';
+  }
+
+  const columnNames =
+    allColumns
+      ? [...data
+        .reduce((columns, row) => { // check each object to compile a full list of column names
+          Object.keys(row).map(rowKey => columns.add(rowKey));
+          return columns;
+        }, new Set())]
+      : Object.keys(data[0]); // just figure out columns from the first item in array
+
+  if (allColumns) {
+    columnNames.sort(); // for predictable order of columns
+  }
+
+  // This will hold data in the format that `async-csv` can accept, i.e.
+  // an array of arrays.
+  let csvInput = [];
+  if (header) {
+    csvInput.push(columnNames);
+  }
+
+  // Add all other rows:
+  csvInput.push(
+    ...data.map(row => columnNames.map(column => row[column])),
+  );
+
+  return await csv.stringify(csvInput);
 }
