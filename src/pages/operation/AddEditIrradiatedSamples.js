3320189e404bcb2aef579353fc04c1af09fca05e
John Preston <byhisdeeds@gmail.com>
2023-12-19 15:46:11 -0500

    Fixed code

--- src/pages/operation/AddEditIrradiatedSamples.js
+++ src/pages/operation/AddEditIrradiatedSamples.js
@@ -351,7 +351,7 @@ class AddEditIrradiatedSamples extends React.Component {
           <Grid item xs={12} style={{paddingLeft: 0, paddingTop: 0}}>
             <TitleComponent title={this.props.title} />
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <IrradiatedSamplesList
               ref={this.listRef}
               title={"Irradiated Samples"}
@@ -362,7 +362,7 @@ class AddEditIrradiatedSamples extends React.Component {
               enableRecordArchiveSwitch={true}
             />
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={9}>
             <IrradiatedSamplesMetadata
               ref={this.metadataRef}
               properties={this.props.properties}
